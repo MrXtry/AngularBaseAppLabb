07eef741d0c9e468399bde730e834d6e2d7c0fb4
kevin larsson <larsson__96@hotmail.com>
2016-09-27 14:06:09 +0200

    .init

---
+++ Scripts/Controllers/main.controller.js
@@ -0,0 +1,30 @@
+﻿/// <reference path="../angular.js" />
+
+angular.module("labbModule")
+    .controller("MainController", [
+        "$scope",
+        "$route",
+        function ($scope, $route) {
+            $scope.$route = $route;
+
+            //$scope.products = [];
+
+            //$scope.loadProducts = function () {
+            //    var dataString = localStorage.getItem("products");
+
+            //    if (dataString)
+            //        $scope.products = JSON.parse(dataString);
+            //}
+
+            //$scope.saveProducts = function () {
+            //    var jsonString = JSON.stringify($scope.products);
+            //    localStorage.setItem("products", jsonString);
+            //}
+
+            $scope.go = function (url) {
+                $location.path(url);
+            }
+
+            //$scope.loadProducts();
+        }
+    ]);
---
+++ Scripts/Routes/route.js
@@ -0,0 +1,37 @@
+﻿/// <reference path="../angular.js" />
+/// <reference path="../angular-route.js" />
+
+angular.module("labbModule")
+    .config([
+        "$routeProvider",
+        "$locationProvider",
+        function ($routeProvider, $locationProvider) {
+            $locationProvider.html5Mode(true);
+            $routeProvider
+                .when("/", {
+                    templateUrl: "Views/Home.html",
+                    controller: "HomeController",
+                    caseInsensitiveMatch: true,
+                    activeTab: "Home"
+                })
+                //.when("/Products", {
+                //    templateUrl: "Views/Products.html",
+                //    controller: "ProductsController",
+                //    caseInsensitiveMatch: true,
+                //    activeTab: "Products"
+                //})
+                //.when("/Products/:id", {
+                //    templateUrl: "Views/Product.html",
+                //    controller: "ProductController",
+                //    caseInsensitiveMatch: true,
+                //    activeTab: "Products"
+                //})
+                //.when("/About", {
+                //    templateUrl: "",
+                //    controller: "AboutController",
+                //    caseInsensitiveMatch: true,
+                //    activeTab: "About"
+                //})
+            ;
+        }
+    ]);
